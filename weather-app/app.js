6c34b02e3b5905f2d0a7ee5bfc00b36154814f45
Pradnya Katigar <pradnya.katigar007@gmail.com>
2022-03-31 17:51:38 +0530

    weather app 1

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
-console.log('Starting')
+const request = require('request')
+const apiCall = 'http://api.weatherstack.com/current?access_key=f92d8c5937b8d7e591b81060543bfc59&query=India'
 
-setTimeout(()=>{
-    console.log('Waiting for 2 seconds')
-},2000)
-console.log('stopping')
+request({url: apiCall},(error, response) => {
+    console.log(response)
+})
