969d8a7aee9d3fc4610ea637109771e96c32c031
pradnya <pradnya.katigar007@gmail.com>
2022-04-05 22:54:22 +0530

    lesson 39

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -9,14 +9,26 @@ console.log(user)
 const product ={
     name:'Red Notebook',
     quantity:10,
-    brand: 'Classmate'
+    brand: 'Classmate',
+    rating:undefined
 }
 
+//Object destructuring
 // const {name,quantity} = product
 // console.log(name)
 // console.log(quantity)
 
-const {name:productname,quantity:productquantity} = product
+//object destructuring with different name and also providing default values
+// const {name:productname,quantity:productquantity,rating=5} = product//
 
-console.log(productname)
-console.log(productquantity)
+
+// console.log(productname)
+// console.log(productquantity)
+// console.log(rating)
+
+
+const transaction = (type,{name,quantity})=>{//Inline destructuring of object
+    console.log(type+ ' '+name+' '+quantity)
+}
+
+transaction('order',product)
