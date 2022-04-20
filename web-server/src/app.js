b463d8d9099f3e30c79188b4f846477789d57a2b
pradnya <pradnya.katigar007@gmail.com>
2022-04-20 22:22:24 +0530

    chapter 48

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,37 @@ const path = require('path')
 const app = express()
 const publicPath = path.join(__dirname,'../public')
 
+//By default hbs looks for /views directory but to customize the directory for hbs do the following:
+const viewsPath = path.join(__dirname,'../templates')
+//setting viewsPath to views
+app.set('views',viewsPath)
 
+
+//setup static directory to serve
 app.use(express.static(publicPath))
-// app.get('',(req,res)=>{
-//     res.send('Welcome!')
-// })
 
-// app.get('/help',(req,res)=>{
-//     app.use(express.static(publicPath+'/help.html'))
-// })
+//setup handlebars engine
+app.set('view engine','hbs')
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'Pradnya Katigar'
+    })
+})
 
-// app.get('/about',(req,res)=>{
-//     res.send('<h1>This is about page<h1>')
-// })
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help Page',
+        name:'Pradnya Katigar'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Page',
+        name:'Pradnya Katigar'
+    })
+})
 
 app.get('/weather',(req,res)=>{
     res.send({
