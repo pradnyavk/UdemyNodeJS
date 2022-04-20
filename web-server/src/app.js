7bd34ff38ad3347f82db67eaf1e5ebc9c8e322b1
Pradnya Katigar <pradnya.katigar007@gmail.com>
2022-04-20 17:25:24 +0530

    section 7 static pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,22 @@
 const express = require('express')
-const path = require()
+const path = require('path')
 
 const app = express()
+const publicPath = path.join(__dirname,'../public')
 
 
-app.get('',(req,res)=>{
-    res.send('Welcome!')
-})
+app.use(express.static(publicPath))
+// app.get('',(req,res)=>{
+//     res.send('Welcome!')
+// })
 
-app.get('/help',(req,res)=>{
-    res.send('Need help?')
-})
+// app.get('/help',(req,res)=>{
+//     app.use(express.static(publicPath+'/help.html'))
+// })
 
-app.get('/about',(req,res)=>{
-    res.send('<h1>This is about page<h1>')
-})
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>This is about page<h1>')
+// })
 
 app.get('/weather',(req,res)=>{
     res.send({
