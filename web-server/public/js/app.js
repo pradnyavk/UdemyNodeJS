b8a54642274419fce94a5c7892f0a62b6ea9e0d4
Pradnya Katigar <pradnya.katigar007@gmail.com>
2022-04-25 18:01:08 +0530

    chap 58

---
+++ web-server/public/js/app.js
@@ -0,0 +1,11 @@
+fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=').then((res)=>{
+    res.json().then((data)=>{
+        console.log(data)
+    })
+})
