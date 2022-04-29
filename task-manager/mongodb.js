622cd4ac9e846f7c0743fb52a9a6da33d0ff8635
Pradnya Katigar <pradnya.katigar007@gmail.com>
2022-04-29 17:32:13 +0530

    chapter 82

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -55,29 +55,77 @@ const databaseName = 'task-manager'
 //     })
 // })
 
+// MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
+//     if(error){
+//        return console.log('Unable to connect')
+//     }
+//     const db = client.db(databaseName)
+//     db.collection('users').findOne({name:'Pradnya'},(error,user)=>{
+//         if(error){
+//             return console.log('Unable to find data')
+//         }
+//         console.log(user)
+//     })
+
+//     db.collection('tasks').find({completed:false}).toArray((error,tasks)=>{
+//         if(error){
+//             return console.log('Unable to find matching data')
+//         }
+//         console.log(tasks)
+//     })
+
+//     db.collection('tasks').findOne({_id:new ObjectId("626ac19b43644478e8fd7e6a")},(error,task)=>{
+//         if(error){
+//             return console.log('unable to find matching data')
+//         }
+//         console.log(task)
+//     })
+// })
+
+
 MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
-    if(error){
-       return console.log('Unable to connect')
-    }
-    const db = client.db(databaseName)
-    db.collection('users').findOne({name:'Pradnya'},(error,user)=>{
         if(error){
-            return console.log('Unable to find data')
+           return console.log('Unable to connect')
         }
-        console.log(user)
-    })
+        const db = client.db(databaseName)
+        // db.collection('users').updateOne({
+        //     _id: ObjectId("626a5d1d3c3e2f31c43020d2")
+        // },{
+        //     $set:{
+        //         name:'Vinita'
+        //     }
+        // }).then((result)=>{
+        //     console.log(result)
+        // }).catch((error)=>{
+        //     console.log(error)
+        // })
 
-    db.collection('tasks').find({completed:false}).toArray((error,tasks)=>{
-        if(error){
-            return console.log('Unable to find matching data')
-        }
-        console.log(tasks)
-    })
+        // db.collection('tasks').updateMany({
+        //     completed:false
+        // },{
+        //     $set:{
+        //         completed:true
+        //     }
+        // }).then((result)=>{
+        //     console.log(result)
+        // }).catch((error)=>{
+        //     console.log(error)
+        // })
 
-    db.collection('tasks').findOne({_id:new ObjectId("626ac19b43644478e8fd7e6a")},(error,task)=>{
-        if(error){
-            return console.log('unable to find matching data')
-        }
-        console.log(task)
+        // db.collection('users').deleteMany({
+        //     name:'Pradnya'
+        // }).then((result)=>{
+        //     console.log(result.deletedCount)
+        // }).catch((error)=>{
+        //     console.log(error)
+        // })
+
+        db.collection('tasks').deleteOne({
+            description:'Ask hr about dependency form'
+        }).then((result)=>{
+            console.log(result.deletedCount)
+        }).catch((error)=>{
+            console.log(error)
+        })
     })
-})
+    
