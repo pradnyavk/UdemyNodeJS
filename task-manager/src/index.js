b4e9de578f8f22d10b633bdfce21b08cdbd06d6d
pradnya <pradnya.katigar007@gmail.com>
2022-05-10 06:55:38 +0530

    section 12 hashing password

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,3 +19,18 @@ app.use(taskRouter)
 app.listen(port,()=>{
     console.log('Server is up on port: '+port)
 })
+
+// const bcrypt = require('bcrypt')
+
+// const myfuction = async()=>{
+//     const password = 'Red12345!'
+//     const hashedPassword = await bcrypt.hash(password,8)
+
+//     console.log('Password:'+password)
+//     console.log('Hashed Password:'+hashedPassword)
+
+//     const isMatch = await bcrypt.compare('red12345!',hashedPassword)
+//     console.log(isMatch)
+// }
+
+// myfuction()
