95b161d8e2c09d6765d19feef3a6260d4005e23b
pradnya <pradnya.katigar007@gmail.com>
2022-05-16 21:18:47 +0530

    auth

--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -1,6 +1,14 @@
+const jwt = require('jsonwebtoken')
+const user = require('../models/user')
+
 const auth = async(req,res,next)=>{
-    console.log('auth middleware')
-    next()
+    try {
+        const token = req.header('Authorization').replace('Bearer ','')
+        const decoded = jwt.verify(token,'thisismynewcourse')
+        console.log(token)
+    } catch (e) {
+        res.status(401).send({error:'Please Authenticate!'})
+    }
 }
 
 module.exports=auth
